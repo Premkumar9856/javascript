0c35b65be02b1b42b8240829c8b47bf228f52c6e
unknown <k.premkumar9856@gmail.com>
2022-05-05 09:59:52 +0530

    quations awnsers

---
+++ index.js
@@ -0,0 +1,111 @@
+// 1. Write a JavaScript program to calculate the volume of a
+// sphere using an arrow function.
+
+// let voluem=(radius)=>{
+// let result=4/3*(22/7)*(radius*radius*radius)
+// console.log(result)
+// }
+
+// voluem(4)
+
+
+
+// 2.Duplicate the segment of a website
+
+// let head=document.querySelector(".heading")
+
+
+// let image=document.querySelector("img");
+
+// img.style.filter="blur(1.3px)";
+
+
+
+// 4
+
+// let a= window.innerHeight;
+// let b= window.innerWidth;
+// let c=document.querySelector("p")
+
+// c.innerHTML="height" + a +"width" + b;
+
+
+// 7. Write a JavaScript program where the program takes a
+// random integer between 1 to 10, the user is then prompted
+// to input a guess number. If the user input matches with the
+// guess number, the program will display a message "Good
+// Work" otherwise display a message "Not matched".
+
+
+// let inputNumber=((a)=>{
+//     if(a>=1 && a<=10){
+
+//         console.log("good work")
+
+//     }else{
+//         console.log("not matched")
+//     }
+// })
+// inputNumber(9);
+
+
+// 3. When a user brings the mouse over the bold text it should be
+// highlighted.
+
+
+// let text=document.querySelector("p");
+
+// text.addEventListener("mouseouer",() =>{
+//    text.style.fontWeight="bold"
+// })
+   
+
+// 11.write a program to covert 1000 minutes in hours.
+
+// let convert=(minutes)=>{
+//  let hours=(minutes/60);
+//  console.log(hours)
+// }
+// convert(1000)
+
+
+
+
+
+
+// 10. Write a javascript program to print “You got A+” if the
+// student has got marks between 91-100, print “You got A” if
+// marks are between 81-90, print “You got B” if marks are
+// between 71-80, print “You got C” if marks are between 41-70
+// and print “You got F” if marks are below 40
+
+// let marks=(mark)=>{
+//     if(mark>91 && mark<=100){
+//         console("You got A+");
+//     }else if(mark>81 && mark<=90){
+//         console("You got A");
+//     }else if(mark>71 && mark<=80){
+//         console("You got B");
+//     }else if(mark>41 && mark<=70){
+//         console.log("You got C");
+//     }else{
+//         console.log("You got F")
+//     }
+// }
+// marks(41);
+
+// 6. Write a JavaScript program to find the area of a triangle
+// where lengths of the three of its sides are 5, 6, 7.
+
+// let x=5;
+// let y=6;
+// let z=7;
+
+// let triangle=((x,y,z)=>{
+//     let s = (x+y+z)/2;
+//     let g= Math.sqrt(s*(s-x)*(s-y)*(s-z));
+    
+//     console.log(g)
+// })
+// triangle(5,6,7)
+
